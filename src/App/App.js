570f70bd2f7ffb8d8fbead466f34e00912363ab0
Susanna Carey <susannaopal@gmail.com>
2022-02-16 12:15:19 -0500

    Set up files and test dummy reservation data

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,41 @@
 import React, { Component } from 'react';
 import './App.css';
+// import {apiCall} from './apiCall.js';
+import './Reservations.js';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      reservations: [
+        {id: 1, 
+        name: "Mary Cat",
+        date: "12/9",
+        time: "7:00",
+        number: 10
+      },
+       {id: 2, 
+        name: "Franny Cat",
+        date: "12/12",
+        time: "5:00",
+        number: 9
+      }
+      ]
+    }
+  }
+
+  // componentDidMount = () => {
+  //   fetchAllReservations()
+  //   .then(data => this.setState({reservations: data.reservations}))
+  // }
+
   render() {
     return (
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
+        <h1 className='testing'>Insert new reservation below</h1>
         <div className='resy-form'>
-
+        
         </div>
         <div className='resy-container'>
           
---
+++ src/App/apiCall.js
@@ -0,0 +1,4 @@
+export const fetchAllReservations = () => {
+  return fetch('http://localhost:3001/api/v1/reservations')
+  .then(response => response.json());
+};
